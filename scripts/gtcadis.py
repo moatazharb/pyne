7163687379f59c4f5950d5fe07d11ecc6777199f
moatazharb <mharb@wisc.edu>
2018-04-04 22:21:04 +0000

    remove quotes from step in config file

--- scripts/gtcadis.py
+++ scripts/gtcadis.py
@@ -22,7 +22,7 @@ config = \
 clean: True
 
 # Assess all materials in geometry for compatibility with SNILB criteria
-'step0':
+step0:
     # Path to hdf5 geometry file for SNILB check. This is the geometry laden
     # file that will be used for activation. Note that this is the same file
     # that will be used for step 2.
@@ -42,7 +42,7 @@ clean: True
 step1: 
 
 # Calculate T matrix for each material
-'step2': 
+step2: 
 
 # Calculate adjoint neutron source
 step3: 
