323fcb9656bcf189c8cdca8563aecfc29106ad15
moatazharb <mharb@wisc.edu>
2018-06-04 23:48:39 +0000

    adding gts calculation to step 2

--- scripts/gtcadis.py
+++ scripts/gtcadis.py
@@ -198,11 +198,11 @@ def step0(cfg, cfg2):
     irr_times = str(cfg2['irr_time']).split(' ')
     decay_times = str(cfg2['decay_time']).split(' ')
     
-    # Define a flat, 175 group neutron spectrum, with magnitude 1E12 [n/s]
-    neutron_spectrum = np.ones(num_n_groups) # will be normalized
-    flux_magnitude = 1.0E12
-    flux_magnitudes = np.array([flux_magnitude * num_n_groups]) # 1E12*175
-
+    # Define a flat, 175 group, neutron spectrum with magnitude 1E12 [n/cm^2.s]
+    group_flux_magnitude = 1.0E12
+    neutron_spectrum = group_flux_magnitude * np.ones(num_n_groups)
+    flux_magnitudes = np.array([np.sum(neutron_spectrum)])
+    
     # Get materials from geometry file
     mat_lib = MaterialLibrary(geom)
     mats = list(mat_lib.values())
@@ -349,11 +349,11 @@ def step2(cfg, cfg2):
     data_dir = cfg2['data_dir']
     irr_times = str(cfg2['irr_time']).split(' ')
     decay_times = str(cfg2['decay_time']).split(' ')
- 
-    # Define a flat, 175 group neutron spectrum, with magnitude 1E12 [n/s]
-    neutron_spectrum = np.ones(num_n_groups) # will be normalized
-    flux_magnitude = 1.0E12
-    flux_magnitudes = np.array([flux_magnitude * num_n_groups]) # 1E12*175
+
+    # Define a flat, 175 group, neutron spectrum with magnitude 1E12 [n/cm^2.s]
+    group_flux_magnitude = 1.0E12
+    neutron_spectrum = group_flux_magnitude * np.ones(num_n_groups)
+    flux_magnitudes = np.array([np.sum(neutron_spectrum)])
     
     # Get materials from geometry file
     mat_lib = MaterialLibrary(geom)
